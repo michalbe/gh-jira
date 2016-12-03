16a6eee77d9b4c772901782040b5f92e79e994bb
Michał Budzyński <michal@virtualdesign.pl>
2016-12-03 17:45:00 +0100

    [GH-JIRA-SOCI] - :horse_racing: get rid of init method

--- chrome/script.js
+++ chrome/script.js
@@ -1,20 +1,17 @@
 'use strict';
 /* global document */
-var linker = (function() {
+var linker = function() {
   var url = 'https://meetsoci.atlassian.net/browse/';
-  var init = function() {
-    var titleDOM = document.querySelector('.js-issue-title');
-    var title = titleDOM.innerHTML;
-    title = title.replace(/(SO-\d+)/gi, function(title) {
-      return '<a href=' + url + title + ' class="jira-link">' + title + '</a>';
-    });
+  var titleDOM = document.querySelector('.js-issue-title');
+  var title = titleDOM.innerHTML;
+  console.log('dom', titleDOM);
+  console.log('title', title);
+  title = title.replace(/(SO-\d+)/gi, function(title) {
+    return '<a href=' + url + title + ' class="jira-link">' + title + '</a>';
+  });
 
-    titleDOM.innerHTML = title;
-  };
+  console.log('title next', title);
+  titleDOM.innerHTML = title;
+};
 
-  return {
-    init: init
-  };
-})();
-
-document.addEventListener('load', linker.init);
+document.addEventListener('load', linker);
