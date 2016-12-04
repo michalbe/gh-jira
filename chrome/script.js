e27e0c929f2a7914c8f1102b239bb380b75256f7
Michał Budzyński <michal@virtualdesign.pl>
2016-12-04 18:06:00 +0100

    [GH-JIRA-SOCI] - :mailbox_with_mail: refresh on reload

--- chrome/script.js
+++ chrome/script.js
@@ -3,15 +3,20 @@
 var linker = function() {
   var url = 'https://meetsoci.atlassian.net/browse/';
   var titleDOM = document.querySelector('.js-issue-title');
-  var title = titleDOM.innerHTML;
-  console.log('dom', titleDOM);
-  console.log('title', title);
-  title = title.replace(/(SO-\d+)/gi, function(title) {
-    return '<a href=' + url + title + ' class="jira-link">' + title + '</a>';
-  });
 
-  console.log('title next', title);
-  titleDOM.innerHTML = title;
+  if (titleDOM) {
+    var title = titleDOM.innerText;
+    title = title.replace(/(SO-\d+)/gi, function(title) {
+      return '<a href=' + url + title + ' class="jira-link" target="_blank">' + title + '</a>';
+    });
+
+    titleDOM.innerHTML = title;
+  }
 };
 
-document.addEventListener('load', linker);
+
+linker();
+
+document
+  .querySelector('div.progress')
+  .addEventListener('transitionend', linker);
